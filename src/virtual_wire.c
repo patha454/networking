f2c049053089b4ec236969ff1a14f5f323975a55
H Paterson <26947190+patha454@users.noreply.github.com>
2021-02-09 01:14:53 +1300

    WIP: Virtual wire.

---
+++ src/virtual_wire.c
@@ -0,0 +1,13 @@
+/**
+ * \file `src/virtual_wire.c`
+ *
+ * `virtual_wire.c` implements a virtual PHY in the form of a simplex channel
+ * between two endpoints.
+ *
+ * \author H Paterson <harley.paterson@postgrad.otago.ac.nz>
+ * \copyright BSL-1.0
+ * \data February 2021
+ */
+
+#include "networking/phy.h"
+
