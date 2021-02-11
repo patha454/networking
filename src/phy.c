21d0094408c5ba4b35a244e523d0a5c2a9140134
H Paterson <26947190+patha454@users.noreply.github.com>
2021-02-11 21:37:50 +1300

    Refactor: Redesign PHY interface.

--- src/phy.c
+++ src/phy.c
@@ -1,10 +1,7 @@
 /**
- * \file `src/phy.c`
- * 
- * `phy.c` defines functions for interacting with a generic PHY interface.
+ * \file include\networking\phy.h
  *
- * \see `include/networking/phy.h` for detailed API specifications and
- * documentation comments.
+ * `phy.h` defines an interface to a simulated physical layer.
  *
  * \author H Paterson <harley.paterson@postgrad.otago.ac.nz>
  * \copyright BSL-1.0
@@ -12,20 +9,53 @@
  */
 
 #include <assert.h>
-#include "networking/phy.h"
+#include <stdlib.h>
 
-size_t phyRead(Phy phy, uint8_t* data, size_t length)
+/**
+ * Maximum number of simulated clients that can be connected to a simulated
+ * PHY.
+ */
+#define MAX_PHY_ENDPOINTS 256
+
+struct Phy
 {
-    assert(phy != NULL);
-    assert(data != NULL);
-    assert(length > 0);
-    return phy->read(phy->context, data, length);
+    
+    /** Number of client endpoints attached to the PHY. */
+    uint clients;
+};
+
+/** Handle to a PHY simulator. */
+typedef struct Phy* Phy;
+
+/**
+ * Allocate a new PHY.
+ *
+ * \return Handle to the new PHY, or NULL on failure.
+ */
+Phy phyAlloc()
+{
+    Phy phy = calloc(1, sizeof(*phy));
+    if (phy == NULL) {
+        return NULL;
+    }
 }
 
-size_t phyWrite(Phy phy, uint8_t* data, size_t length)
+/**
+ * Free an existing PHY.
+ *
+ * \note `phyFree` will not perform any teardown work.
+ *
+ * \param phy The PHY to destroy.
+ */
+void phyFree(Phy phy)
 {
+    free(phy);
+}
+
+int main(void)
+{
+    Phy phy = phyAlloc();
     assert(phy != NULL);
-    assert(data != NULL);
-    assert(length > 0);
-    return phy->write(phy->context, data, length);
+    phyFree(phy);
+    return EXIT_SUCCESS;
 }
