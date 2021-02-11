25d7ace575b05777d45febc40a45e40bf5c3364c
H Paterson <26947190+patha454@users.noreply.github.com>
2021-02-11 22:02:47 +1300

    WIP: PHY implementation.

--- src/phy.c
+++ src/phy.c
@@ -9,6 +9,7 @@
  */
 
 #include <assert.h>
+#include <sys/socket.h>
 #include <stdlib.h>
 
 /**
@@ -22,6 +23,9 @@ struct Phy
     
     /** Number of client endpoints attached to the PHY. */
     uint clients;
+
+    /** File descriptors for client sockets. */
+    int sockets[MAX_PHY_ENDPOINTS];
 };
 
 /** Handle to a PHY simulator. */
@@ -38,6 +42,17 @@ Phy phyAlloc()
     if (phy == NULL) {
         return NULL;
     }
+    return phy;
+}
+
+/**
+ * Configure a newly allocated PHY for use.
+ *
+ * \param phy The Phy to configure.
+ */
+void phyConfigure(Phy phy)
+{
+    
 }
 
 /**
@@ -52,10 +67,32 @@ void phyFree(Phy phy)
     free(phy);
 }
 
+/**
+ * Connect a socket endpoint to the virtual PHY.
+ *
+ * \param phy The PHY to add a new connection to.
+ * \return File descriptor to read and write to the PHY, or -1 on error.
+ */
+int phyConnect(Phy phy)
+{
+    assert(phy != NULL);
+    if (phy->clients == MAX_PHY_ENDPOINTS) {
+        return -1;
+    }
+    int endpoints[2] = { 0 };
+    if (socketpair(AF_UNIX, SOCK_STREAM, 0, endpoints) == -1) {
+        return -1;
+    } 
+    phy->sockets[phy->clients++] = endpoints[0];
+    return endpoints[1];
+}
+
 int main(void)
 {
     Phy phy = phyAlloc();
     assert(phy != NULL);
+    int fd = phyConnect(phy);
+    assert(fd != -1);
     phyFree(phy);
     return EXIT_SUCCESS;
 }
