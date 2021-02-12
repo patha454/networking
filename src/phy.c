09be8b01cd565c2fc5b2e4be2290f5a8a13ec1b9
H Paterson <26947190+patha454@users.noreply.github.com>
2021-02-12 18:30:02 +1300

    WIP: Debug phy propagation.

--- src/phy.c
+++ src/phy.c
@@ -11,6 +11,7 @@
 #include <assert.h>
 #include <sys/epoll.h>
 #include <sys/socket.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -20,6 +21,16 @@
  * PHY.
  */
 #define MAX_PHY_ENDPOINTS 256
+
+/**
+ * Maximum number of pending epoll events to return per scan.
+ */
+#define MAX_EPOLL_EVENTS 16
+
+/**
+ * Size, in bytes, of a buffer for copying from sockets.
+ */
+#define PHY_BUFFER_SIZE 4096
 
 struct Phy
 {
@@ -32,6 +43,9 @@ struct Phy
 
     /** File descriptors for client sockets. */
     int sockets[MAX_PHY_ENDPOINTS];
+
+    /** Pending I/O events on the sockets. */
+    struct epoll_event events[MAX_EPOLL_EVENTS];
 };
 
 /** Handle to a PHY simulator. */
@@ -74,8 +88,6 @@ void phyConfigure(Phy phy)
 int phyConnect(Phy phy)
 {
     struct epoll_event events = { 0 };
-    events.events = EPOLLIN;
-    events.data.fd = phy->clients;
     assert(phy != NULL);
     if (phy->clients == MAX_PHY_ENDPOINTS) {
         perror("Too many PHY clients");
@@ -85,7 +97,9 @@ int phyConnect(Phy phy)
     if (socketpair(AF_UNIX, SOCK_STREAM, 0, endpoints) == -1) {
         perror("Create PHY socketpair");
         exit(EXIT_FAILURE);
-    } 
+    }
+    events.events = EPOLLIN;
+    events.data.fd = endpoints[0];
     if (epoll_ctl(phy->epoll, EPOLL_CTL_ADD, endpoints[0], &events) == -1) {
         perror("Register epoll on FD");
         exit(EXIT_FAILURE);
@@ -95,12 +109,76 @@ int phyConnect(Phy phy)
 }
 
 /**
+ * Helper method for `phyPropagateFromSocket`. Write the full contets of a buffer to
+ * a socket, or dies trying.
+ *
+ * \param socketFd Socket to write to.
+ * \param buffer Buffer to write.
+ * \param length Length of the `buffer` to write.
+ */ 
+void writeToSocket(int socketFd, uint8_t* buffer, size_t length)
+{
+    size_t sizeWritten = 0;
+    ssize_t result = 0;
+    while (sizeWritten < length) {
+        printf("socket %d\nsizeWritten %ld\nlength %ld\n", socketFd, sizeWritten, length);
+        result = write(socketFd, buffer + sizeWritten, length - sizeWritten);
+        if (result == -1) {
+            perror("Write to PHY socket");
+            exit(EXIT_FAILURE);
+        }
+        sizeWritten += result;
+    }
+}
+
+/**
+ * Helper method for `phyPropagate`. Echos all output from one socket into all
+ * other sockets.
+ *
+ * \param phy The phy to propagate over.
+ * \param socketFd The socket to propagate from.
+ */
+void phyPropagateFromSocket(Phy phy, int socketFd)
+{
+    unsigned int i = 0;
+    ssize_t sizeRead = 0;
+    uint8_t* buffer = malloc(PHY_BUFFER_SIZE);
+    if (buffer == NULL) {
+        perror("Allocate propagation buffer.");
+        exit(EXIT_FAILURE);
+    }
+    while (true) {
+        sizeRead = read(socketFd, buffer, PHY_BUFFER_SIZE);
+        if (sizeRead == 0) {
+            break;
+        }
+        if (sizeRead == -1) {
+            perror("Read from PHY socket");
+            exit(EXIT_FAILURE);
+        }
+        printf("sizeRead: %ld\n", sizeRead);
+        for (i = 0; i < phy->clients; i++) {
+            if (phy->sockets[i] == socketFd) {
+                continue;
+            }
+            writeToSocket(phy->sockets[i], buffer, sizeRead);
+        }
+    }
+}
+
+/**
  * Propagates data over the PHY, if any input data is pending from clients.
  *
  * \param Phy The phy to propagate data over.
  */
 void phyPropagate(Phy phy)
 {
+    int pendingEvents = 0;
+    int i = 0;
+    pendingEvents = epoll_wait(phy->epoll, phy->events, MAX_EPOLL_EVENTS, 0);
+    for (i = 0; i < pendingEvents; i++) {
+        phyPropagateFromSocket(phy, phy->events[i].data.fd);
+    }
     return;
 }
 
@@ -144,8 +222,13 @@ int main(void)
     Phy phy = phyAlloc();
     assert(phy != NULL);
     phyConfigure(phy);
-    int fd = phyConnect(phy);
-    assert(fd != -1);
+    int fd1 = phyConnect(phy);
+    int fd2 = phyConnect(phy);
+    assert(fd1 != -1);
+    assert(fd2 != -1);
+    write(fd1, "Hello, network", 15);
+    printf("Write to fd %d\n", fd1);
+    phyPropagate(phy);
     phyFree(phy);
     return EXIT_SUCCESS;
 }
